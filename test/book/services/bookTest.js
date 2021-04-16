9eac66a231218fb05888af1a0ccc6fd52f998bd9
Scott Adams <sradms0@protonmail.com>
2021-04-16 16:01:40 -0400

    update services.book.readAll tests to check for ascending book return (fail)

--- test/book/services/bookTest.js
+++ test/book/services/bookTest.js
@@ -24,15 +24,24 @@ describe('services.book.readAll', () => {
     expect(bookService.readAll() instanceof Promise).to.be.true;
   });
 
-  it('it should return a Promise resolving to an array of all books', async () => {
-    const books = await bookService.readAll();
-    expect(books.length).to.eql(bookData.length);
-    books.forEach(book => expect(book instanceof Book).to.be.true);
-  });
-
   it('it should return a Promise resolving to an array of all Book instances', async () => {
     const books = await bookService.readAll();
     books.forEach(book => expect(book instanceof Book).to.be.true);
+  });
+
+  it('it should return should return all books in ascending title-order', async () => {
+    const books = await bookService.readAll({ order: ['title', 'ACS'] });
+    const sortedBookData = [...bookData]
+      .sort((b1, b2) => {
+            const b1L = b1.title.toLowerCase(), 
+                  b2L = b2.title.toLowerCase();
+            if (b1L < b2L) return -1;
+            if (b1L > b2L) return 1;
+            return 0;
+          });
+    books.forEach((dbBook, idx) => 
+      expect(dbBook.title).to.eql(sortedBookData[idx].title)
+    );
   });
 });
 
