14b8c9cc3e1edd9d3d7be2d8a62b3e8a1db1fb4d
Scott Adams <sradms0@protonmail.com>
2021-04-23 14:04:04 -0400

    update controllers.book.readByPk tests: replace chai-http use with mock-req-res (fail)

--- test/book/controllers/bookTest.js
+++ test/book/controllers/bookTest.js
@@ -3,35 +3,47 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
+
 const { loader } = require('$seed/books');
 const { testOperations: testOps } = require('$test/lib');
 const { expect } = chai;
 const server = require('$root/app');
 
+const { book: bookController } = require('$controllers');
+const { book: bookService } = require('$services');
 const { sequelize } = require('$database/models');
 const { models: {Book} } = sequelize;
 
-chai.use(require('chai-http'));
+const { stub, match } = require('sinon')
+const { mockRequest, mockResponse } = require('mock-req-res')
+const proxyquire = require('proxyrequire')
 
+chai.use(require('sinon-chai'));
+chai.use(require('chai-as-promised'));
 
 describe('controllers.book.readByPk', () => {
-  let requester;
-
   before('reload', async () => {
     await testOps.loadTestDb();
-    requester = await chai.request(server).keepOpen();
   });
-  after('close', () => {
-    requester.close();
+  
+  it('it should render book/update and pass one book object', async () => {
+    const res = mockResponse(),
+          id = 1,
+          req = mockRequest({ params: {id} }),
+          book = await bookService.readByPk(id);
+
+    await bookController.readByPk(req, res);
+    expect(res.render).to.have.been.calledWith('book/update', { book });
   });
 
-  it('it should set res.status to 200 when a book is found', async () => {
-    const res = await requester.get('/books/1');
-    expect(res).to.have.status(200);
-  });
-  it('it should set res.status to 404 when a book is not found', async () => {
-    const res = await requester.get('/books/1000');
-    expect(res).to.have.status(404);
+  it('it should throw an error when a non-existent book is requested', async () => {
+    const res = mockResponse(),
+          id = -1,
+          req = mockRequest({ params: {id} }),
+          book = await bookService.readByPk(id);
+
+    expect(await bookController.readByPk(req, res, err => err.message))
+      .to.equal(`Book with id ${id} does not exist`);
   });
 });
 
