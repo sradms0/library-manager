4f2b7244746bc11b0465a2c2b11511127c61511e
Scott Adams <sradms0@protonmail.com>
2021-04-28 12:23:41 -0400

    force db syncing to re-create tables pre-seeding to avoid row duplicates

--- seed/index.js
+++ seed/index.js
@@ -7,6 +7,6 @@ const bookSeeder = require('./books');
 const { data: {books, genres}, loader: bookLoader } = bookSeeder;
 
 asyncHandler(async () => {
-  await sequelize.sync();
+  await sequelize.sync({ force: true });
   await bookLoader.load(books, genres);
 })();
