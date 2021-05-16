482ff8d9cd12798e1357f4047a95adb749f666db
Scott Adams <sradms0@protonmail.com>
2021-05-16 11:54:39 -0400

    fix typos in in views.book.delete tests and add routes/view for tests (fail)

--- routes/book.js
+++ routes/book.js
@@ -9,6 +9,12 @@ const router = express.Router();
 /* GET books listing. */
 router.get('/', bookController.readAll);
 
+/* GET one book for deletion. */
+router.get('/:id/delete', bookController.readDelete);
+
+/* POST book for deletion. */
+router.post('/:id/delete', bookController.delete);
+
 /* GET one book. */
 router.get('/:id/update', bookController.readByPk);
 
--- test/book/views/bookTest.js
+++ test/book/views/bookTest.js
@@ -426,12 +426,12 @@ describe('views.book.update', () => {
 
 describe('views.book.delete', () => {
   const browser = new Browser();
-  let book, id, requester;
+  let book, form, id, requester;
 
   beforeEach('', async () => {
     await testOps.loadTestDb();
     requester = await chai.request(server).keepOpen(),
-    book = (await bookService.readByPk(id))?.[0],
+    book = (await bookService.readAll())?.[0],
     id = book ? book.id : -1;
     await testOps.Route.visitOneBookDel(browser, id);
     form = browser.querySelector('form');
@@ -452,7 +452,7 @@ describe('views.book.delete', () => {
 
   it('it should display a form with an action of /books/:id/delete', () => {
     const [ action ] = form?.action?.match(/\/books\/\d+\/delete$/g);
-    expect(action).to.equall(`/books/${id}/delete`);
+    expect(action).to.equal(`/books/${id}/delete`);
   });
 
   it('it should display a button to submit the delete-book form', () => {
@@ -460,7 +460,7 @@ describe('views.book.delete', () => {
     expect(submitI).to.not.be.null;
   });
 
-  it('it should have a cancel link that brings the user back to /books', async () => {
+  it('it should have a cancel link that brings the user back to /books/:id/update', async () => {
     const extractRoute = url => url?.match(/\/books\/(\d+)\/update$/g);
     const cancelA = browser.querySelector('a.button');
     await browser.clickLink(cancelA);
@@ -473,7 +473,7 @@ describe('views.book.delete', () => {
   it('it should submit the form deleting an existing book', async () => {
     form.submit();
     await browser.wait();
-    testOps.Route.visitBooks(browser);
+    await testOps.Route.visitBooks(browser);
     const noBookA = [...browser.querySelectorAll('td a')].find(a => a.textContent === book.title);
     expect(noBookA).to.be.undefined;
   });
