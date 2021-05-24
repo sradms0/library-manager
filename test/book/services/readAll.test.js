2b075b1642958395450464e0793e25fa2a09bb18
Scott Adams <sradms0@protonmail.com>
2021-05-24 12:13:38 -0400

    update services.book.readAll tests: add expectations for limiting and offsetting books (fail)

--- test/book/services/readAll.test.js
+++ test/book/services/readAll.test.js
@@ -17,18 +17,85 @@ describe('services.book.readAll', () => {
     expect(bookService.readAll() instanceof Promise).to.be.true;
   });
 
-  it('it should return a Promise resolving to an array of all Book instances', async () => {
-    const books = await bookService.readAll();
-    books.forEach(book => expect(book instanceof bookService.model).to.be.true);
+  it('it should return a Promise resolving to an object with a book total', async () => {
+    const { count } = await bookService.readAll();
+    expect(count).to.equal(testOps.Data.book.length);
+  });
+
+  it('it should return a Promise resolving to an object with an array of Book instances', async () => {
+    const { rows } = await bookService.readAll();
+    rows.forEach(book => expect(book instanceof bookService.model).to.be.true);
   });
 
   it('it should return all books from the database', async () => {
-    const dbBooks = await bookService.readAll(),
+    const { count: dbBookCount, rows: dbBooks} = await bookService.readAll(),
           rawBooks = testOps.Data.book;
 
-    const matched = dbBooks.length === rawBooks.length && 
+    const matched = dbBookCount === rawBooks.length && 
                     rawBooks.every((rb, idx) => rb.title === dbBooks[idx].title);
     expect(matched).to.be.true;
+  });
+
+  describe('limit and offset', async () => {
+    let allBooks, limitedBooks, 
+        offset = 0, limit = 0;
+
+    before('create more instances for pagination', async () => {
+      const total = 20;
+      for (let i = 0; i < total; i++) {
+        await bookService.create({ 
+          title: `title ${i}`,
+          author: `author ${i}`,
+          genre: `genre ${i}`,
+          year: i
+        });
+      }
+      ({ rows: allBooks } = await bookService.readAll());
+    });
+
+    describe('limit', async () => {
+      it('it should return a limit of one book', async () => {
+        limit = 1;
+        let [ { title: firstTitle } ] = allBooks?.slice(offset,limit);
+        let { rows: [ {title: limitedFirstTitle} ]} = await bookService.readAll({ offset, limit });
+        expect(firstTitle).to.equal(limitedFirstTitle);
+      });
+
+      it('it should return a limit of some books', async () => {
+        limit = Math.ceil(allBooks.length/2);
+        const someBooks = allBooks?.slice(offset,limit);
+        limitedBooks = await bookService.readAll({ offset, limit });
+        someBooks.forEach((book, idx) =>  expect(book.title === limitedBooks.rows[idx].title));
+      });
+
+      it('it should return a limit of no books', async () => {
+        limit = offset = 0;
+        let { rows: noBooks } = await bookService.readAll({ offset, limit });
+        expect(noBooks).to.be.empty;
+      });
+    });
+
+    describe('offset', async () => {
+      it('it should return a limit of one book with an offset greater than zero', async () => {
+        offset = limit = 1;
+        const [ { title: secondTitle } ] = allBooks?.slice(offset,limit+1);
+        const { rows: [ {title: limitedSecondTitle} ] } = await bookService.readAll({ offset, limit });
+        expect(secondTitle).to.equal(limitedSecondTitle);
+      });
+
+      it('it should return a limit of some books with an offset greater than zero', async () => {
+        limit = Math.ceil(allBooks.length/2);
+        const someBooks = allBooks?.slice(limit+1);
+        limitedBooks = await bookService.readAll({ offset, limit });
+        someBooks.forEach((book, idx) => expect(book.title === limitedBooks.rows[idx].title));
+      });
+
+      it('it should return a limit of no books with an offset ...', async () => {
+        limit = 0, offset = 1;
+        let { rows: noBooks } = await bookService.readAll({ offset, limit });
+        expect(noBooks).to.be.empty;
+      });
+    });
   });
 });
 
