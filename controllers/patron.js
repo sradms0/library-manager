dea3445130cbf1f9025a7b88c1b842a2af3a088b
Scott Adams <sradms0@protonmail.com>
2021-05-29 15:42:28 -0400

    pass controllers.patron.readAll tests

--- controllers/patron.js
+++ controllers/patron.js
@@ -15,4 +15,8 @@ const {
  * Reads all patrons and renders all books to '/views/patron/index'
  *
 */
-exports.readAll = asyncHandler(async function(req, res) {});
+exports.readAll = asyncHandler(async function(req, res) {
+  assertParams('patrons', res, req);
+  const renderConf = await readDataAndCreateRenderConf('patrons', patronService.readAll, req, '/patrons?');
+  res.render('patron/index', renderConf);
+});
