bb598d0aad4e82e3357428b72f13002f8d0b3b1d
Scott Adams <sradms0@protonmail.com>
2021-06-06 12:27:11 -0400

    update controllers.book.readByAttrs tests: refactor `it` search tests to loops (tests still pass)

--- test/book/controllers/readByAttrs.test.js
+++ test/book/controllers/readByAttrs.test.js
@@ -19,56 +19,38 @@ chai.use(require('sinon-chai'));
 
 
 describe('controllers.book.readByAttrs', () => {
+  const modelAttrs = testOps.Data.getModelAttrs(bookService.model, { without: ['id', 'createdAt', 'updatedAt'] });
+
   before('reload', async () => {
     await testOps.loadTestDb('book');
   });
 
   describe('one book result', async () => {
-    let books, title, author, genre, year;
+    let books;
     let page, limit;
-
     before('', async () => {
       books = (await bookService.readAll()).rows.slice(0,1);
-      if (books.length) {
+      if (books.length) 
         books = [ await bookService.update(books[0], {genre: 'very unique'}) ];
-        ({ title, author, genre, year } = books?.[0]);
-      }
     });
 
-    it('it should render one searched book by its title', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: title} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render one searched book by its author', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: author} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render one searched book by its genre', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: genre} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render one searched book by its year', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: year} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
+    modelAttrs.forEach(attr => {
+      it(`it should render one searched book by its ${attr}`, async () => {
+        const res = mockResponse(),
+              req = mockRequest({ query: {q: books[0][attr]} });
+        await bookController.readByAttrs(req, res);
+        expect(res.render).to.have.been.calledWith('book/index', { books });
+      });
+    })
   });
 
   describe('many book results', async () => {
-    const title = 'title',
-          author = 'author',
-          genre = 'genre',
-          year = 'year';
+    const nonUniqueData = {
+      title : 'title',
+      author : 'author',
+      genre : 'genre',
+      year : '1000'
+    };
 
     const books = [];
 
@@ -76,37 +58,18 @@ describe('controllers.book.readByAttrs', () => {
       const totalSimilar = 5
       for (let i = 0; i < totalSimilar; i++) {
         // need to fetch after creation since ordering of attrs differ...
-        const { id } = await bookService.create({ title, author, genre, year });
+        const { id } = await bookService.create(nonUniqueData);
         books.push( await bookService.readByPk(id) );
       }
     });
 
-    it('it should render many books by title', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: title} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render many books by author', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: author} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render many books by genre', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: genre} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
-    });
-
-    it('it should render many books by year', async () => {
-      const res = mockResponse(),
-            req = mockRequest({ query: {q: year} });
-      await bookController.readByAttrs(req, res);
-      expect(res.render).to.have.been.calledWith('book/index', { books });
+    modelAttrs.forEach(attr => {
+      it(`it should render many searched books by their ${attr}`, async () => {
+        const res = mockResponse(),
+              req = mockRequest({ query: {q: nonUniqueData[attr]} });
+        await bookController.readByAttrs(req, res);
+        expect(res.render).to.have.been.calledWith('book/index', { books });
+      });
     });
   });
 
