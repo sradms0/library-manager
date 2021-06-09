d7c48a79ebc0c430da9133a170c8cb8b0bdd8f83
Scott Adams <sradms0@protonmail.com>
2021-06-09 14:14:08 -0400

    pass controllers.manager.home tests

--- controllers/manager.js
+++ controllers/manager.js
@@ -5,7 +5,9 @@
 */
 
 /**
- * Sends default pagination configurtation, rendering /views/index
+ * Sends default pagination configurtation for sub-root links, rendering /views/index
 */
-exports.home = function(req, res) {}
+exports.home = function(req, res) {
+  res.render('index', { page: 1, limit: 10 });
+}
 
