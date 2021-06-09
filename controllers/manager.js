60af709e38d90d78c230f6dbcdda93d4d3dbd487
Scott Adams <sradms0@protonmail.com>
2021-06-09 14:45:30 -0400

    pass updated controllers.manager.home test

--- controllers/manager.js
+++ controllers/manager.js
@@ -8,6 +8,6 @@
  * Sends default pagination configurtation for sub-root links, rendering /views/index
 */
 exports.home = function(req, res) {
-  res.render('index', { page: 1, limit: 10 });
+  res.render('manager/index', { page: 1, limit: 10 });
 }
 
