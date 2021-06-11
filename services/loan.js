0ef834d8c558a4fb979b2fd711c4761b6fbba3ce
Scott Adams <sradms0@protonmail.com>
2021-06-11 17:19:36 -0400

    pass services.loan.readAll tests

--- services/loan.js
+++ services/loan.js
@@ -12,9 +12,10 @@ const { Book, Loan, Patron } = require('$database/models');
  * @param { number } config.limit - the amount of loans to read.
  * @param { number } config.offset - where the loan-reading should begin.
  * @returns { Promise }
- *
 */
-exports.readAll = function({ limit, offset }={}) {};
+exports.readAll = function({ limit, offset }={}) {
+  return Loan.findAndCountAll({ include: [Book, Patron], limit, offset });
+};
 
 /** Quick access to model */
 exports.model = Loan;
