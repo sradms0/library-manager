b46bc8147fcad10a19f9ecdbb9654cdec201cdd2
Scott Adams <sradms0@protonmail.com>
2021-06-13 12:46:25 -0400

    pass controllers.loan.readAll tests

--- controllers/loan.js
+++ controllers/loan.js
@@ -15,4 +15,8 @@ const {
  * Reads all loans and renders all loans to '/views/loan/index'
  *
 */
-exports.readAll = asyncHandler(async function(req, res) {});
+exports.readAll = asyncHandler(async function(req, res) {
+  assertParams('loans', res, req);
+  const renderConf = await readDataAndCreateRenderConf('loans', loanService.readAll, req, '/loans?');
+  res.render('loan/index', renderConf);
+});
