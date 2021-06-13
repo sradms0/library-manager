0d57b94b46a59e6c226dc94a01a759bc6b02a746
Scott Adams <sradms0@protonmail.com>
2021-06-13 12:45:32 -0400

    add controllers.loan.readAll tests (fail)

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,7 @@
 */
 module.exports = {
   book: require('./book'),
+  loan: require('./loan'),
   manager: require('./manager'),
   patron: require('./patron'),
   error: require('./error')
---
+++ controllers/loan.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @module controllers/loan
+*/
+
+const { loan: loanService } = require('$services');
+const { 
+  errorHandling: {assertFind, asyncHandler},
+  pagination: {assertParams, readDataAndCreateRenderConf}
+} = require('$root/lib');
+
+
+/**
+ * Reads all loans and renders all loans to '/views/loan/index'
+ *
+*/
+exports.readAll = asyncHandler(async function(req, res) {});
