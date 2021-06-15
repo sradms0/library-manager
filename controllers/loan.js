48a18de227c89131c1d2d86430cb175e3395217e
Scott Adams <sradms0@protonmail.com>
2021-06-15 12:16:22 -0400

    pass controllers.loan.readByPk tests

--- controllers/loan.js
+++ controllers/loan.js
@@ -26,7 +26,12 @@ exports.readAll = asyncHandler(async function(req, res) {
  * Sets `res.status` to 404 when a loan is not found.
  *
 */
-exports.readByPk = asyncHandler(async function(req, res) {});
+exports.readByPk = asyncHandler(async function(req, res) {
+  const { id } = req.params;
+  const loan = await loanService.readByPk(id);
+  assertFind(loan, 'Loan', id);
+  res.render('loan/update', { dataValues: loan });
+});
 
 /**
  * Reads one loan by primary key and renders loan to '/views/loan/return' for return confirmation.
