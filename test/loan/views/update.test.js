894ab758e14683dc889a0fc088b0e4275ad8dd17
Scott Adams <sradms0@protonmail.com>
2021-07-07 14:46:38 -0400

    update views.loan.update tests: add expectation for link to loan-deletion (fail)

--- test/loan/views/update.test.js
+++ test/loan/views/update.test.js
@@ -87,6 +87,16 @@ describe('views.loan.update', () => {
     expect(urlRoute).to.equal(cancelAHrefRoute);
   });
 
+  it('it should have a delete link that brings the user to /loans/:id/delete', async () => {
+    const extractRoute = url => url.match(/\/loans\/\d+\/delete$/g);
+    const deleteA = browser.querySelector('a#delete');
+    await browser.clickLink(deleteA);
+
+    const [ deleteAHrefRoute ] = extractRoute(deleteA?.href),
+          [ urlRoute ] = extractRoute(browser.location._url);
+    expect(urlRoute).to.equal(deleteAHrefRoute);
+  });
+
   it('it should submit the form, updating the existing loan', async () => {
     const updatedCopy = { id, ...updated };
 
