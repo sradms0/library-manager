5352d8fec029abcb36600b7413d249a7bbd3cab2
Scott Adams <sradms0@protonmail.com>
2021-07-20 10:00:07 -0400

    add views.book.index.filters tests (fail)

---
+++ test/book/views/index.filters.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.env.NODE_ENV = 'test';
+
+const { asyncUtil: {asyncForEach} } = require('$root/lib');
+const Browser = require('zombie');
+const chai = require('chai');
+const { testOperations: testOps } = require('$test/lib');
+const { expect } = chai;
+const server = require('$root/bin/www');
+
+const { book: bookService } = require('$services');
+
+
+chai.use(require('chai-http'));
+
+
+describe('views.book.index', () => {
+  const browser = new Browser(),
+        filters = ['all', 'checked-out', 'overdue'],
+        pagination = '\\?page=1&limit=10';
+
+  let requester;
+
+  before('reload', async () => {
+    await testOps.Data.loadTestDb('book');
+  });
+
+  beforeEach('start server', async () => {
+    requester = await chai.request(server).keepOpen();
+    await testOps.Route.visitBooks(browser);
+  });
+
+  afterEach('close server', () => {
+    requester.close();
+  });
+
+  
+  for(let i = 0; i < filters.length; i++) {
+    const filter = filters[i];
+    it(`it should show a(n) ${filter}-books filter link`, async () => {
+      const extractRoute = url => url?.match(new RegExp(`/books/${filter}${pagination}$`, 'g')),
+            filterA = browser.querySelector(`#filter-${filter}`),
+            [ filterAHref ] = extractRoute(filterA?.href);
+
+      await browser.clickLink(filterA);
+      const [route] = extractRoute(browser.location._url);
+      expect(filterAHref).to.equal(route);
+    });
+  }
+});
